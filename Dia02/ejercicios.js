a3ba303f8040598aedb283eb44e14251abd1a616
AronJustoC <aronchoque727@gmail.com>
2026-03-07 22:11:03 -0500

    feat: implement arrays and objects functions

--- Dia02/ejercicios.js
+++ Dia02/ejercicios.js
@@ -13,25 +13,60 @@ for (const enunciado of enunciados) {
 }
 
 function declararSaludo(nombre) {
-  return "TODO";
+  return `Hola, ${nombre}!!!`;
 }
 
 function sumarArray(numeros) {
-  return "TODO";
+  let suma = 0;
+  for (const valor of numeros) {
+    suma += valor;
+  }
+  return suma;
 }
 
 function filtrarPares(numeros) {
-  return "TODO";
+  let newArray = [];
+  for (const valor of numeros) {
+    if (valor % 2 === 0) {
+      newArray.push(valor);
+    }
+  }
+  return newArray;
 }
 
 function contarPropiedades(objeto) {
-  return "TODO";
+  let contador = 0;
+  for (const key in objeto) {
+    contador++;
+  }
+  return contador;
 }
 
 function combinarNombres(personas) {
-  return "TODO";
+  let response = [];
+  for (const persona of personas) {
+    response.push(persona.nombre);
+  }
+  return response.join(", ")
 }
 
 function buscarPorId(lista, id) {
-  return "TODO";
+  for (const persona of lista) {
+    if (persona.id == id) {
+      return persona;
+    }
+  }
 }
+
+//console.log(combinarNombres([{nombre: "Ana"}, {nombre: "Beto"}, {nombre: "Cata"}]));
+//console.log(buscarPorId([{id: 1, nombre: "Ana"}, {id: 2, nombre: "Beto"}],99));
+
+console.log("\n--- TESTS ---");
+console.log("sumarArray([1,2,3,4]):", sumarArray([1, 2, 3, 4]));
+console.log("filtrarPares([1,2,3,4]):", filtrarPares([1, 2, 3, 4]));
+console.log("contarPropiedades({a:1, b:2}):", contarPropiedades({a: 1, b: 2}));
+console.log("combinarNombres([{nombre:'Ana'},{nombre:'Beto'}]):", combinarNombres([{nombre: "Ana"}, {nombre: "Beto"}]));
+console.log("buscarPorId([{id:1},{id:2}], 2):", buscarPorId([{id: 1, nombre: "Ana"}, {id: 2, nombre: "Beto"}], 2));
+console.log("buscarPorId([{id:1}], 99):", buscarPorId([{id: 1}], 99));
+
+
